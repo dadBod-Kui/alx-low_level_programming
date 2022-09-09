25bc7c2e4dc3c212c7dc6e90ed75824f7562bd74
dadBod-Kui <samf4600@gmail.com>
2022-09-09 12:43:30 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/** This a code that prints a line and directs it to the standard error output. 
+ *
+ *  * The code returns 1
+ *
+ *   * */
+
+int main()
+
+{
+
+		putchar("and that piece of art is useful\" -Dora Korpar, 2015-10-19,");
+
+
+
+}
+
+return(1);
