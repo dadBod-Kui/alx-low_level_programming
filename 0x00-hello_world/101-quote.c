1a127a8534af964b6d55441783e82dcff7387bdb
dadBod-Kui <samf4600@gmail.com>
2022-09-09 12:47:17 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 
 /** This a code that prints a line and directs it to the standard error output. 
  *
- *  * The code returns 1
+ *  * The code returns 0
  *
  *   * */
 
@@ -12,10 +12,10 @@ int main()
 
 {
 
-		putchar("and that piece of art is useful\" -Dora Korpar, 2015-10-19,");
+		printf("and that piece of art is useful\" -Dora Korpar, 2015-10-19,");
 
 
 
 }
 
-return(1);
+return(0);
